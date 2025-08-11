bf61faf50189c716ddf07c10b7e5e76d54b4c8ef
Taner Saydam <taner_and_you@hotmail.com>
2025-08-11 12:58:54 +0300

    Aspire da PostgreSQL container olarak ayağa kaldıralım

--- Aspire.AppHost/AppHost.cs
+++ Aspire.AppHost/AppHost.cs
@@ -2,8 +2,12 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+#region Variables
+var userName = builder.AddParameter("SqlUserName", "postgres", true);
 var password = builder.AddParameter("SqlPassword", "Password12*", true);
+#endregion
 
+#region Product
 var mssqlDb = builder.AddSqlServer("SqlServer")
     .WithLifetime(ContainerLifetime.Persistent)
     .WithHostPort(1233)
@@ -15,7 +19,17 @@ var productDb = mssqlDb.AddDatabase("ProductDb");
 builder.AddProject<Aspire_ProductWebAPI>("product")
     .WithReference(productDb)
     .WaitFor(productDb);
+#endregion
 
-builder.AddProject<Aspire_CategoryWebAPI>("category");
+#region Category
+var postgreDb = builder.AddPostgres("Postgres", userName, password, 5432)
+    .WithLifetime(ContainerLifetime.Persistent);
+var categoryDb = postgreDb.AddDatabase("categorydb");
+
+builder.AddProject<Aspire_CategoryWebAPI>("category")
+    .WithReference(categoryDb)
+    .WaitFor(categoryDb);
+#endregion
+
 
 builder.Build().Run();
