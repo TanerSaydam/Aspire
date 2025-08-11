070a76935a059050fdabd898125505e5836e8892
Taner Saydam <taner_and_you@hotmail.com>
2025-08-11 12:31:49 +0300

    Aspire da MSSQL container olarak ayağa kaldıralım

--- Aspire.AppHost/AppHost.cs
+++ Aspire.AppHost/AppHost.cs
@@ -2,7 +2,20 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Aspire_ProductWebAPI>("product");
+var password = builder.AddParameter("SqlPassword", "Password12*", true);
+
+var mssqlDb = builder.AddSqlServer("SqlServer")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithHostPort(1233)
+    .WithPassword(password)
+    ;
+
+var productDb = mssqlDb.AddDatabase("ProductDb");
+
+builder.AddProject<Aspire_ProductWebAPI>("product")
+    .WithReference(productDb)
+    .WaitFor(productDb);
+
 builder.AddProject<Aspire_CategoryWebAPI>("category");
 
 builder.Build().Run();
